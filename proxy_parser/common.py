469fa08148594470fd7601b0549a20d16e026cb8
Alexander Nesterov <alex19pov31@gmail.com>
2020-08-03 23:27:29 +0300

    init repo

---
+++ proxy_parser/common.py
@@ -0,0 +1,384 @@
+from urllib import request
+from urllib.parse import urlencode
+from urllib.error import HTTPError, URLError
+from http.client import HTTPResponse
+from html.parser import HTMLParser
+from typing import List, Tuple, Dict, Union
+from datetime import datetime, timedelta
+import socket
+import pickle
+import hashlib
+import os
+import csv
+import json
+import asyncio
+
+__all__ = ['Proxy', 'ProxyList']
+
+class CacheManager():
+    def __init__(self, storage_path: str = './'):
+        self.storage_path = storage_path
+
+    def __get_cache_file(self, key: str):
+        return os.path.join(self.storage_path, f'{key}.pickle')
+
+    def save(self, data, key: str, time_diff: timedelta):
+        """Сохраняем кеш"""
+        cached_data: dict = {
+            'data': data,
+            'key': key,
+            'time_exp': datetime.now() + time_diff
+        }
+
+        file_path: str = self.__get_cache_file(key)
+        dir_name: str = os.path.dirname(file_path)
+        if not os.path.isdir(dir_name):
+            os.makedirs(dir_name, exist_ok=True)
+
+        with open(file_path, 'wb') as f:
+            pickle.dump(cached_data, f)
+
+    def get(self, key: str):
+        """Запрашиваем данные по ключу"""
+        data = None
+        if not self.check(key): 
+            return None
+
+        file_path: str = self.__get_cache_file(key)
+        with open(file_path, 'rb') as f:
+            cached_data: dict = pickle.load(f)
+            data = cached_data.get('data', None)
+        
+        return data
+
+    def check(self, key: str) -> bool:
+        """Проверяем наличие актуального кеша"""
+        is_success: bool = False
+        file_path: str = self.__get_cache_file(key)
+        if not os.path.isfile(file_path): 
+            return False
+
+        with open(file_path, 'rb') as f:
+            cached_data: dict = pickle.load(f)
+            time_exp = cached_data.get('time_exp', None)
+            if time_exp is not None and time_exp >= datetime.now():
+                is_success = True
+            else:
+                os.remove(file_path)
+
+        return is_success
+
+class HttpRequestResult():
+    def __init__(
+            self, 
+            responce = None, 
+            error: Exception = None, 
+            body: str = None, 
+            status: int = None, 
+            headers = None):
+        self.responce: HTTPResponse = responce
+        self.error = error
+        self.__body: str = body
+        self.__status: int = status
+        self.__headers = headers
+
+    def is_success(self):
+        if not self.responce:
+            return False
+
+        return 200 >= self.__status < 300
+
+    def has_error(self) -> bool:
+        if self.error:
+            return True
+
+        return False
+
+    @property
+    def headers(self):
+        return self.__headers
+
+
+    def get_body(self, encoding: str = 'utf-8') -> str:
+        if not self.__body:
+            return ''
+
+        return self.__body.decode(encoding)
+
+class HttpClient():
+    headers: Dict[str, str] = {
+        'Accept-Encoding': '',
+        'Accept': 'gzip, deflate',
+        'Accept-Language': 'ru',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'DNT': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:79.0) Gecko/20100101 Firefox/79.0',
+    }
+
+    def __init__(self, cache_time_diff: timedelta = None, cache_storage_dir: str = './'):
+        self.cache_manager: CacheManager = CacheManager(cache_storage_dir)
+        self.cache_time_diff = cache_time_diff
+
+    def _prepare_headers(self, headers: Dict[str, str]) -> Dict[str, str]:
+        result = self.headers
+        for key, value in headers.items():
+            result[key] = value
+
+        return result
+
+    def __internal_http_request(self, req: request.Request, cache_key: str, timeout: float = None):
+        if not timeout:
+            timeout = socket._GLOBAL_DEFAULT_TIMEOUT
+        with request.urlopen(req, timeout=timeout) as resp:
+            resp: HTTPResponse = request.urlopen(req)
+            body: str = resp.read()
+            resp.close()
+            
+            data: HttpRequestResult = HttpRequestResult(
+                    responce=resp, 
+                    status=resp.status, 
+                    body=body, 
+                    headers=resp.headers.items())
+
+            return self._save_cache(cache_key, data)
+            
+        return None
+
+    def _get_cache_key(self, url: str, method: str, data = None, headers: Dict[str, str] = {}) -> str:
+        return hashlib.md5(pickle.dumps({
+            'url': url,
+            'method': method,
+            'data': data,
+            'headers': headers
+        })).hexdigest()
+
+    def _get_cache(self, cache_key: str) -> HttpRequestResult:
+        if self.cache_time_diff is not None and self.cache_manager.check(cache_key):
+            return self.cache_manager.get(cache_key)
+        
+        return None
+
+    def _save_cache(self, cache_key: str, result: HttpRequestResult) -> HttpRequestResult:
+        if self.cache_time_diff is not None:
+                self.cache_manager.save(result, cache_key, self.cache_time_diff)
+        return result
+
+    def http_request(self, url: str, method: str, data = None, headers: Dict[str, str] = {}, timeout: float = None) -> HttpRequestResult:
+        """HTTP запрос"""
+        cache_key = self._get_cache_key(url, method, data, headers)
+        if data:
+            data = urlencode(data).encode('ascii')
+
+        cache_result: HttpRequestResult = self._get_cache(cache_key)
+        if cache_result is not None:
+            return cache_result
+        
+        headers = self._prepare_headers(headers)
+        req: request.Request = request.Request(url, headers=headers, data=data, method=method)
+        request_result: HttpRequestResult = None
+
+        try:
+            request_result = self.__internal_http_request(req, cache_key)
+        except (HTTPError, URLError) as request_error:
+            request_result = HttpRequestResult(error=request_error)
+        
+        return request_result
+
+    def http_get_request(self, url: str, headers: Dict[str, str] = {}, timeout: float = None) -> HttpRequestResult:
+        """HTTP GET запрос"""
+        return self.http_request(url, 'GET', None, headers, timeout)
+
+    def http_post_request(self, url: str, data = None, headers: Dict[str, str] = {}, timeout: float = None) -> HttpRequestResult:
+        """HTTP POST запрос"""
+        return self.http_request(url, 'POST', data, headers, timeout)
+
+    def http_put_request(self, url: str, data = None, headers: Dict[str, str] = {}, timeout: float = None) -> HttpRequestResult:
+        """HTTP PUT запрос"""
+        return self.http_request(url, 'PUT', data, headers, timeout)
+
+    def http_delete_request(self, url: str, data = None, headers: Dict[str, str] = {}, timeout: float = None) -> HttpRequestResult:
+        """HTTP DELETE запрос"""
+        return self.http_request(url, 'DELETE', data, headers, timeout)
+
+class Proxy(HttpClient):
+    fields: List[str] = [
+        'type',
+        'addr',
+        'port',
+        'login',
+        'password',
+        'time_add',
+        'time_check'
+    ]
+
+    def __init__(self, 
+            proxy_type: str, 
+            addr: str, 
+            port: int, 
+            login: str = None, 
+            password: str = None,
+            time_add: datetime = datetime.now(),
+            time_check: datetime = None,
+            **kwargs):
+        self.type: str = proxy_type
+        self.addr: str = addr
+        self.port: int = port
+        self.latency: float = 0
+        self.login = login
+        self.password = password
+        self.__checked_latency: float = 0
+        self.__time_add: datetime = time_add 
+        self.__time_check: datetime = time_check
+        self.__time_use: datetime = None
+
+    def __str__(self) -> str:
+        return '%s://%s:%s' % (self.type, self.addr, self.port)
+
+    def get_host(self) -> str:
+        return '%s:%s' % (self.addr, self.port)
+
+    @property
+    def time_add(self) -> datetime:
+        return self.__time_add
+
+    @property
+    def time_check(self) -> datetime:
+        """Время проверки прокси-сервера"""
+        return self.__time_ckeck
+
+    async def check(self, timeout_sec: float = 1):
+        """Проверка соединения с прокси-сервером"""
+        try:
+            self.__time_check = datetime.now()
+            conn = asyncio.open_connection(self.addr, self.port)
+            await asyncio.wait_for(conn, timeout=timeout_sec)
+            conn.close()
+            self.__checked_latency = timeout_sec
+        except (asyncio.TimeoutError, ConnectionRefusedError, OSError):
+            self.__checked_latency = 0
+
+
+    @staticmethod
+    def from_dict(data: dict):
+        proxy_type: str = data.get('type', '')
+        addr: str = data.get('addr', '')
+        port: int = int(data.get('port', 0))
+        login: str = data.get('login', None)
+        password: str = data.get('password', None)
+
+        return Proxy(proxy_type, addr, port, login=login, password=password)
+
+    def to_dict(self) -> dict:
+        return {
+            'type': self.type,
+            'addr': self.addr,
+            'port': self.port,
+            'login': self.login,
+            'password': self.password,
+            'time_add': self.__time_add,
+            'time_check': self.__time_check
+        }
+
+    def in_timeout(self, timeout: float = 1) -> bool:
+        return self.__checked_latency > 0 and self.__checked_latency <= timeout
+        
+class ProxyList(list):
+    mode: str = 'normal'
+
+    def append(self, item): 
+        if isinstance(item, Proxy):
+            super().append(item)
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            lst = super().__getitem__(item)
+            return self.__class__(lst)
+        else: 
+            return super().__getitem__(item)
+
+    def filter(self, **kwargs):
+        """Фильтрация прокси-серверов"""
+        result: ProxyList = ProxyList()
+        result.set_mode(self.mode)
+        type_proxy: str = kwargs.get('type', 'all')
+        checked_timeout: float = float(kwargs.get('checked_timeout', 0))
+
+        for proxy in self:
+            if type_proxy.lower() != 'all' and proxy.type.lower() != type_proxy.lower():
+                continue
+            if checked_timeout > 0 and not proxy.in_timeout(checked_timeout):
+                continue
+            
+            result.append(proxy)
+
+        return result
+    
+    def set_mode(self, mode: str):
+        """Режим перебора прокси-серверов"""
+        self.mode: str = mode
+
+    def sort(self, **kwargs):
+        pass
+
+    def __str__(self):
+        return '\n'.join([str(proxy) for proxy in self])
+
+    def load_csv(self, file_name: str):
+        """Загрузка списка из csv"""
+        with open(file_name, 'r') as f:
+            csv_reader = csv.DictReader(f, fieldnames=Proxy.fields, delimiter=';')
+            for item in csv_reader:
+                self.append(Proxy.from_dict(item))
+
+    def to_dict(self) -> List[dict]:
+        """Конвертация в словарь"""
+        return [item.to_dict() for item in self]
+
+    def load_json(self, file_name: str):
+        """Загрузка списка из json"""
+        with open(file_name, 'r') as f:
+            json_data: Dict = json.load(f)
+            for item in json_data:
+                self.append(Proxy.from_dict(item))
+
+    def dump_csv(self, file_name: str):
+        """Сохранение списка в csv"""
+        with open(file_name, 'w') as f:
+            csv_writer = csv.DictWriter(f, delimiter=';', fieldnames=Proxy.fields)
+            for item in self.to_dict():
+                csv_writer.writerow(item)
+
+    def dump_json(self, file_name: str):
+        """Сохранение списка в json"""
+        with open(file_name, 'w') as f:
+            json.dump(self.to_dict(), f)
+
+    def check(self, timeout: float = 1):
+        """Проверка соединения с прокси-серверами"""
+        event_loop = asyncio.get_event_loop()
+        task_list: list = []
+        for proxy in self:
+            task = event_loop.create_task(proxy.check(timeout))
+            task_list.append(task)
+
+        event_loop.run_until_complete(asyncio.wait(task_list))
+
+        return self
+
+class CommonProxyParser(HTMLParser):
+    headers: Dict[str, str] = {}
+
+    def __init__(self):
+        self.http_client: HttpClient = HttpClient(cache_time_diff=timedelta(hours=1), cache_storage_dir='.cache/')
+        self.proxy_list: ProxyList = ProxyList()
+        self.addr = ''
+        self.type = ''
+        self.port = 0
+        super().__init__()
+
+    def get_html(self, url: str, encoding: str = 'utf-8') -> str:
+        return self.http_client.http_get_request(url, headers=self.headers).get_body(encoding=encoding)
+
+    def __str__(self):
+        return str(self.proxy_list)
---
+++ proxy_parser/parser.py
@@ -0,0 +1,304 @@
+from .common import CommonProxyParser, Proxy, ProxyList
+from typing import List, Tuple, Dict
+from enum import Enum, Flag, unique, auto
+import base64
+import json
+
+@unique
+class FreeProxyCzSort(Enum):
+    PING = 'ping'
+    SPEED = 'speed'
+    UPTIME = 'uptime'
+    DATE = 'date'
+
+@unique
+class FreeProxyCzType(Enum):
+    HTTP = 'http'
+    HTTPS = 'https'
+    SOCKS = 'socks'
+    SOCKS4 = 'socks4'
+    SOCKS5 = 'socks5'
+    ALL = 'all'
+
+@unique
+class FreeProxyCzLevel(Enum):
+    LEVEL1 = 'level1'
+    LEVEL2 = 'level2'
+    LEVEL3 = 'level3'
+    ALL = 'all'
+
+class FreeProxyCzParser(CommonProxyParser):
+    """Список прокси с сайта http://free-proxy.cz"""
+    url: str = 'http://free-proxy.cz/en/proxylist/country/%s/%s/%s/%s/%s'
+    
+    def __init__(
+            self, 
+            pages: List[int], 
+            sort: FreeProxyCzSort = FreeProxyCzSort.PING,
+            protocol: FreeProxyCzType = FreeProxyCzType.ALL,
+            level: FreeProxyCzLevel = FreeProxyCzLevel.ALL,
+            country: str = 'all'):
+        self.sort = sort.value
+        self.level = level.value
+        self.protocol = protocol.value
+        self.country = country
+        self.parse_table: bool = False
+        self.parse_port: bool = False
+        self.parse_type: bool = False
+        self.new_proxy: bool = True
+
+        super().__init__()
+        self.headers['Host'] = 'free-proxy.cz'
+        for page in pages:
+            self.html = self.get_html_page(page)
+            self.feed(self.html)
+
+    def get_html_page(self, page: int) -> str:
+        """Запрос страницы со списком прокси-серверов"""
+        params: set = (self.country, self.protocol, self.sort, self.level, page)
+        return self.get_html(self.url % params)
+
+    def handle_starttag(self, tag, attrs):
+        """Обработка открывающих тегов"""
+        if tag == 'table' and ('id', 'proxy_list') in attrs: 
+            self.parse_table = True
+
+        if not self.parse_table:
+            return
+
+        if tag == 'span' and ('class', 'fport') in attrs:
+            self.parse_port = True
+        else:
+            self.parse_port = False
+
+        if tag == 'small':
+            self.parse_type = True
+        else:
+            self.parse_type = False
+
+
+    def handle_data(self, data: str):
+        """Обработка данных (текст)"""
+        if not self.parse_table:
+            return
+
+        if 'document.write(Base64.decode("' in data:
+            enc_addr: str = data.replace('document.write(Base64.decode("', '').replace('"))', '').encode('ascii')
+            self.addr = base64.decodebytes(enc_addr).decode('ascii')
+            self.new_proxy = True
+
+        if self.parse_port:
+            self.port = int(data)
+
+        if self.new_proxy and self.parse_type:
+            self.proxy_list.append(Proxy(data.lower(), self.addr, self.port))
+            self.new_proxy = False
+
+class FreeProxyListNetParser(CommonProxyParser):
+    url: str = 'https://free-proxy-list.net/'
+
+    def __init__(self):
+        self.parse_list: bool = False
+        super().__init__()
+        self.headers['Host'] = 'free-proxy-list.net'
+        self.html = self.get_html(self.url)
+        self.feed(self.html)
+
+    def handle_starttag(self, tag, attrs):
+        """Обработка открывающих тегов"""
+        if tag == 'textarea' and ('class', 'form-control') in attrs \
+                and ('readonly', 'readonly') in attrs and ('rows', '12') in attrs \
+                and ('onclick', 'select(this)') in attrs: 
+            self.parse_list = True
+
+    def handle_data(self, data: str):
+        """Обработка данных (текст)"""
+        raw_list_header: str = 'Free proxies from free-proxy-list.net'
+        if self.parse_list and raw_list_header in data:
+            self.parse_list = False
+            raw_list: str = data.replace(raw_list_header, '')
+            proxy_list: List[str] = raw_list.split()
+            for raw_proxy in proxy_list[5:]:
+                addr, port = raw_proxy.split(':')
+                self.proxy_list.append(Proxy('http', addr, int(port)))
+
+
+class SpysOneParser(CommonProxyParser):
+    url: str = 'http://spys.one/en/free-proxy-list/'
+
+    def __init__(self):
+        self.const_list: Dict[str, int] = {}
+        self.current_addr: str = ''
+        self.current_port: int = 0
+        self.is_https: bool = False
+        self.row_start: bool = False
+        self.parse_addr: bool = False
+        self.parse_port: bool = False
+        self.parse_type: bool = False
+        self.start_parse_constants: bool = False
+        self.finish_parse_constants: bool = False
+        super().__init__()
+        self.headers['Host'] = 'free-proxy-list.net'
+        self.html = self.get_html_page()
+        self.feed(self.html)
+
+    def get_html_page(self) -> str:
+        """Запрос страницы со списком прокси-серверов"""
+        post_data: Dict[str, int] = {
+            'xpp': 5,
+            'xf1': 0,
+            'xf2': 0,
+            'xf4': 0,
+            'xf5': 0,
+        }
+        request_result: HttpRequestResult = self.http_client.http_post_request(self.url, data=post_data)
+
+        return request_result.get_body()
+
+    def handle_starttag(self, tag, attrs):
+        """Обработка открывающих тегов"""
+        if not self.finish_parse_constants and tag == 'script' and ('type', 'text/javascript') in attrs: 
+            self.start_parse_constants = True
+
+        if tag == 'tr' and ('class', 'spy1xx') in attrs:
+            self.row_start = True
+
+        if self.row_start and tag == 'font' and ('class', 'spy14') in attrs:
+            self.parse_addr = True
+
+        if self.row_start and tag == 'a' and ('href', '/en/https-ssl-proxy/') in attrs:
+            self.is_https = True
+
+        if self.row_start and tag == 'font' and ('class', 'spy1') in attrs:
+            self.parse_type = True
+            self.row_start = False
+
+        if self.parse_addr and tag == 'script' and ('type', 'text/javascript') in attrs:
+            self.parse_port = True
+
+    def __parse_constants(self, data: str):
+        """Парсинг констант для вычисления порта"""
+        const_expression: List[str] = data.split(';')
+        for raw_expr in const_expression:
+            const_expr_list: List[str] = raw_expr.split('=')
+            if len(const_expr_list) < 2:
+                continue
+
+            const_name, const_expr = const_expr_list
+            expr_list: List[str] = const_expr.split('^')
+            num: int = int(expr_list[0])
+            if len(expr_list) > 1: 
+                other_const_name: str = expr_list[1]
+                other_const: int = self.const_list.get(other_const_name, 0)
+                self.const_list[const_name] = num ^ other_const
+            else: 
+                self.const_list[const_name] = num
+
+        self.start_parse_constants = False
+        self.finish_parse_constants = True
+
+    def __parse_port(self, data: str):
+        """Парсинг порта"""
+        data = data.replace('document.write("<font class=spy2>:<\/font>"+', '')
+        expr_list = data.split('+')
+        port_num_list: List[str] = []
+        for raw_expr in expr_list:
+            raw_expr = raw_expr.replace('(', '').replace(')', '')
+            num_list = raw_expr.split('^')
+            num1_name = num_list[0]
+            num1 = self.const_list[num1_name]
+            if len(num_list) > 1:
+                num2_name = num_list[1]
+                num2 = self.const_list[num2_name]
+                port_num_list.append(str(num1^num2))
+            else:
+                port_num_list.append(str(num1))
+
+        self.current_port = int(''.join(port_num_list))
+        self.parse_addr = False
+        self.parse_port = False
+
+    def handle_data(self, data: str):
+        """Обработка данных (текст)"""
+        if self.start_parse_constants:
+            self.__parse_constants(data)
+
+        if not self.parse_port and self.parse_addr:
+            self.current_addr = data
+
+        if self.parse_port:
+            self.__parse_port(data)
+
+        if self.parse_type:
+            if self.is_https:
+                proxy_type: str = 'https'
+            else:
+                proxy_type: str = data.lower()
+            self.proxy_list.append(Proxy(proxy_type, self.current_addr, self.current_port))
+            self.parse_type = False
+            self.is_https = False
+
+class ProxyScrapeType(Flag):
+    HTTP = auto()
+    SOCKS4 = auto()
+    SOCKS5 = auto()
+    ALL = HTTP|SOCKS4|SOCKS5
+
+class ProxyScrapeParser(CommonProxyParser):
+    url: str = 'https://api.proxyscrape.com/?request=getproxies&proxytype=%s&timeout=10000&country=all&ssl=all&anonymity=all'
+
+    def __init__(self, protocol: ProxyScrapeType = ProxyScrapeType.ALL):
+        super().__init__()
+        for i, proxy_type in enumerate(['http', 'socks4', 'socks5']):
+            current_protocol: int = (1<<i)
+            if protocol.value & current_protocol != current_protocol:
+                continue
+
+            url = self.url % proxy_type
+            request_result = self.http_client.http_get_request(url)
+            if not request_result or not request_result.is_success():
+                continue
+
+            raw_proxy_list = request_result.get_body().split()
+            for raw_proxy in raw_proxy_list:
+                proxy_data = raw_proxy.split(':')
+                if len(proxy_data) < 2: 
+                    continue
+
+                addr, str_port = proxy_data
+                self.proxy_list.append(Proxy(proxy_type, addr, int(str_port)))
+
+class ProxyListDownloadType(Flag):
+    HTTP = auto()
+    HTTPS = auto()
+    SOCKS4 = auto()
+    SOCKS5 = auto()
+    ALL = HTTP|HTTPS|SOCKS4|SOCKS5
+
+class ProxyListDownloadParser(CommonProxyParser):
+    url: str = 'https://www.proxy-list.download/api/v0/get?l=en&t=%s'
+
+    def __init__(self, protocol: ProxyListDownloadType = ProxyListDownloadType.ALL):
+        self.parse_textarea_list: bool = False
+        super().__init__()
+        self.headers['Host'] = 'www.proxy-list.download'
+        for i, proxy_type in enumerate(['http', 'https', 'socks4', 'socks5']):
+            current_protocol: int = (1<<i)
+            if protocol.value & current_protocol != current_protocol:
+                continue
+
+            url = self.url % proxy_type
+            request_result = self.http_client.http_get_request(url)
+            if not request_result.is_success():
+                continue
+
+            data = request_result.get_body()
+            json_data = json.loads(data)
+            json_proxy_list: List[Dict] = json_data[0].get('LISTA', [])
+            for item in json_proxy_list:
+                addr = item.get('IP', None)
+                port_str = item.get('PORT', None)
+                if not addr or not port_str:
+                    continue
+
+                self.proxy_list.append(Proxy(proxy_type, addr, int(port_str)))
